9aa114ca5130301064fbfff7877f03935bf894a4
donv05 <donv051994@gmail.com>
2020-10-09 14:54:45 +0700

    update

---
+++ variable.py
@@ -0,0 +1,24 @@
+# this is the first comment
+the_word_is_flat = True # and this is the second comment
+                        # .. comment
+if the_word_is_flat:
+    print('Be careful not fall off!')
+
+2 + 2
+
+50 -5*6
+
+# Lists
+squares = [1, 4, 9, 16, 25]
+squares[0]
+
+#Fibonacci series
+## the sum of two element defines the next
+a, b = 0, 1
+while a < 10:
+    print(a)
+    a, b = b, a + b
+
+# String
+# formatting with the .format() method
+# a good way to format object into your strings print statements is with the string .format() method. the syntax    
